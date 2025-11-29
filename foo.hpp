0427f25c63646e9a7b152bfa43aa0582c14c341a
Michał Tomporowski <michalang400@gmail.com>
2025-11-29 14:34:55 +0100

    proba 9

--- foo.hpp
+++ foo.hpp
@@ -9,7 +9,7 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-    std::list< Human >::iterator it = people.rbegin();
+    std::list< Human >::iterator it = people.begin();
     auto porownanie = [it]() { return *it++.isMonster() ? 'n' : 'y'; };
     std::for_each(retval.begin(), retval.end(), porownanie);
 
