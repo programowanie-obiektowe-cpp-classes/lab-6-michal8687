dd1c5de266945a4b208680d450876041a2a2ff76
Michał Tomporowski <michalang400@gmail.com>
2025-11-29 14:31:59 +0100

    proba 7

--- foo.hpp
+++ foo.hpp
@@ -9,9 +9,7 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-    std::vector< Human >::iterator it = people.rbegin();
-
-    // Twoja implementacja tutaj
+    std::vector< Human >::iterator it = people.end();
     auto porownanie = [it]() { return *it++.isMonster() ? 'n' : 'y'; };
     std::for_each(retval.begin(), retval.end(), porownanie);
 
